91a9587d38e53c92b080382b28ce59ad7239f6d9
shiauapple <shiauapple@gmail.com>
2017-08-19 20:44:42 +0800

    new

---
+++ js/new.js
@@ -0,0 +1,27 @@
+var fdb = new ForerunnerDB();
+var db =fdb.db("myDB");
+var newCollection =db.collection('new')
+
+newCollection.load();
+
+$('#submit').click(function(){
+	var date = $("#date").val();
+	var category = $("#category").val();
+	var item = $("#item").val();
+	var cost = $("#cost").val();
+
+
+	newCollection.insert({
+		date:date,
+		category:category,
+		item:item,
+		cost:cost
+		});
+	newCollection.save();
+	alert("儲存成功")
+
+	$("#date").val("");
+	$("#category").val("");
+	$("#item").val("");
+	$("#cost").val("");
+	})
